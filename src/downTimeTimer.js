5b86afa9cb284adda8b5d216811b7120a89dfdbe
skywing77 <blieviolett@gmail.com>
2024-11-07 16:27:12 +0900

    Update downTimeTimer.js
    txt에 버전 저장하고 불러오기

--- src/downTimeTimer.js
+++ src/downTimeTimer.js
@@ -3,16 +3,85 @@
 import cron from 'node-cron';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import readline from 'readline';
+import { error } from 'console';
 
 dotenv.config();
 
 let version;
 let message;
+const versionPath = 'downTime.txt';
+
+function versionFileCheck(){
+  fs.access(versionPath, fs.constants.F_OK, e => {
+    if(e){
+      console.log('파일이 존재하지 않습니다.');
+      fs.open(versionPath, 'w', (e,fd) => {
+        if(e){
+          console.log('파일 생성중 오류가 발생했습니다.');
+        }else{
+          console.log(`${versionPath}파일이 생성되었습니다!`);
+        }
+        fs.close(fd, () => {          
+        })
+      })
+    }
+  })
+}
+
+function versionCheck(){
+  let lastline;
+  const stream = fs.createReadStream(versionPath, 'utf8')
+  const rl = readline.createInterface({
+    input:stream,
+  })
+
+  rl.on('line', line => {
+    lastline = line;
+  })
+
+  rl.on('close', ()=> {
+    version = lastline;
+    //업데이트 확인
+    if (!version) {
+      //서버 버전이 없는 경우
+      message = {
+        content: `서버 ON , 서버 버전을 확인 할 수 없습니다.${serverStatus.server_version}버전을 최신 버전으로 설정 했습니다.`,
+      };
+      version = serverStatus.server_version;
+      //새로 저장된 버전 파일에 추가
+      fs.appendFile(versionPath, '\n' + version.toString(), e => {
+        if(e){
+          console.error(e);
+        }
+      })
+    } else if (version < serverStatus.server_version) {
+      //서버 버전이 다른 경우
+      message = {
+        content: '서버 ON, 버전이 업데이트 되었습니다. 미꾸라지 유저는 주의해주세요',
+      };
+      version = serverStatus.server_version;
+      //파일에 버전 내용 추가
+      fs.appendFile(versionPath, '\n' + version.toString(), e => {
+        if(e){
+          console.error(e);
+        }
+      })
+    } else {
+      message = {
+        //별다른 이슈가 없을때,서버 오픈 메세지
+        content: '서버 ON',
+      };
+    }
+    console.log(`${version}이 서버 버전으로 설정되었습니다.`)
+  })
+}
 
 const downTimeTracker = () => {
   console.log('DT 타이머 등록완료');
   cron.schedule('0 11 * * *', async () => {
     console.log('DT 타이머 실행');
+    versionFileCheck(); // 버전 파일 존재 여부 체크
     try {
       const interval = setInterval(async () => {
         const response = await fetch('https://esi.evetech.net/latest/status/?datasource=tranquility');
@@ -30,33 +99,7 @@ const downTimeTracker = () => {
 
         if (currentDate === startTimeDate) {
           //서버 열린상태 확인됬을때 코드
-
-          //업데이트 확인
-          if (!version) {
-            //서버 버전이 없는 경우
-            message = {
-              content: `서버 ON , 서버 버전을 확인 할 수 없습니다.${serverStatus.server_version}버전을 최신 버전으로 설정 했습니다.`,
-            };
-            version = serverStatus.server_version;
-          } else if (version < serverStatus.server_version) {
-            message = {
-              //서버 버전이 다른 경우
-              content: '서버 ON, 버전이 업데이트 되었습니다. 미꾸라지 유저는 주의해주세요',
-            };
-            version = serverStatus.server_version;
-          } else {
-            message = {
-              //별다른 이슈가 없을때,서버 오픈 메세지
-              content: '서버 ON',
-            };
-          }
-          //서버 버전 정보 txt 파일로 저장
-          fs.appendFile('downTimeReport.txt', `${startTime} : ${version}`, e => {
-            if(e){
-              console.error(e);
-            }
-            console.log(`${startTime} : ${version} 이 데이터에 추가되었습니다.`);
-          });
+          versionCheck();
           fetch(process.env.DISCORD_WEBHOOK_URL, {
             method: 'POST',
             headers: {
