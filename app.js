71c239a6705fab42684ff8ea7297895ee4d7e21c
skywing77 <blieviolett@gmail.com>
2024-11-08 23:24:53 +0900

    interaction 이벤트 리스너 추가

--- app.js
+++ app.js
@@ -68,4 +68,26 @@ client.on('messageCreate', async msg => {
   }
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 client.login(process.env.DISCORD_BOT_TOKEN);
