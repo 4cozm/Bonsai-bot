f392f41e6c08648a98c9c536c8e9c31b09a96777
4cozm <17328rm@gmail.com>
2024-11-13 02:21:56 +0900

    ♻️ 예외처리 추가

---
+++ src/utils/serverStartNotification.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const serverStartNotification = async startTime => {
+  const time = Date.now() - startTime;
+  let message = { content: `서버가 재시작 되었습니다! 재부팅에 소요된 시간: ${time}ms` };
+
+  try {
+    await fetch(process.env.DISCORD_404_WEBHOOK_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(message),
+    });
+  } catch (error) {
+    console.error('Webhook 전송 실패:', error);
+  }
+};
+
+export default serverStartNotification;
