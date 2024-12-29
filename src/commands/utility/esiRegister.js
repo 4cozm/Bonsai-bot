adf8342f0451edb83fb16c2ce79d321af531bd8f
4cozm <17328rm@gmail.com>
2024-12-29 17:57:05 +0900

    🍻

--- src/commands/utility/esiRegister.js
+++ src/commands/utility/esiRegister.js
@@ -1,7 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { addReplyMessage, createState } from '../../esi/stateManager.js';
 import getCustomError from '../../errors/index.js';
-import { getClientInstance } from '../../utils/discordClientManger.js';
 import { getMessageId, deleteState } from '../../esi/stateManager.js';
 
 const { dataNotFoundError } = await getCustomError();
@@ -23,18 +22,13 @@ export async function execute(interaction) {
     content: `고유 번호가 포함되어 있습니다. 다른 사람에게 링크를 공유하지 말아주세요!\n\n[>>ESI 등록 링크<<](http://cat4u.store:3000/esi/signUp?state=${state})\n`,
     ephemeral: true,
   });
-  console.log('replyMessage의 정체', replyMessage);
   addReplyMessage(state, replyMessage); //메세지 객체를 추가로 저장함 -> 나중에 댓글로 성공 여부를 알려주기 위함임
 }
 
 export const updateRegistrationMessage = async (state, message) => {
   try {
-    const client = getClientInstance();
     const { messageId, channelId } = getMessageId(state);
-    console.log('채널ID', channelId);
-    const channel = await client.channels.fetch(channelId);
-    const msg = await channel.messages.fetch(messageId);
-    await msg.edit({ content: message });
+    await messageId.edit({ content: message });
     deleteState(state);
   } catch (error) {
     console.error('updateRegistrationMessage에서 오류 발생', error);
