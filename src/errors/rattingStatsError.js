9083c4365af962631932afc04d36b9a0128f0f24
skywing77 <blieviolett@gmail.com>
2025-01-03 02:25:41 +0900

    Update rattingStatsError.js

--- src/errors/rattingStatsError.js
+++ src/errors/rattingStatsError.js
@@ -1,10 +1,10 @@
 import customError from './customError.js';
 
-class c5RattingStatsError extends customError {
+class rattingStatsError extends customError {
   constructor(data, message = '5클조업 통계 오류') {
     super(message);
     this.data = data;
   }
 }
 
-export default c5RattingStatsError;
+export default rattingStatsError;
